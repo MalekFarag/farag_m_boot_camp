4270ea8ccafcae76ea69e3ab368cf3405b03faf2
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-09-15 13:30:21 -0400

    continued css (team section) & started js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 (() => {
-    console.log('Fired.');
+    console.log('Fired.');'
     
+    const mateInfo = ["Hey, I’m Nick!<br>I’m a website designer and front end developer for Cliff Creative’s. I’m a creative mind that focuses heavily on branding with logo design’s, style guides, photography, videography, 3D and more, with some HTML5 and CSS on the side. In my free time I love to play basketball, work on and drive cars, as well as portrait and automotive photography.", "Hey, I’m Collins!<br>I’m a Project Manager at Cliff Creatives. Bringing an idea to life is no walk in the park. It is my job to make sure expectations are met on both sides - the client and the company. I’m a huge soccer fan (Go Manchester United) and in my spare time I love to cook and watch old sitcoms." ,"Hey, I’m Malek!<br>I’m the back-end developer at Cliff Creatives. I love producing projects for the company! We have lots of fun in the office!I like to spend time with friends, trying new things and exploring new places!"]
+    ;
+    // mateInfo 1 Nick, 2 Collins, 3 Malek;
     })();
