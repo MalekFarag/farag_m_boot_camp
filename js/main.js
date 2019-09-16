9967acc1c01a600b09a070e07d6c75ff1194a815
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-09-16 16:23:32 -0400

    bio function complete (css needs work) (portfolio page needed)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
 (() => {
-    console.log('Fired.');'
+    console.log('Fired.');
     
-    const mateInfo = ["Hey, I’m Nick!<br>I’m a website designer and front end developer for Cliff Creative’s. I’m a creative mind that focuses heavily on branding with logo design’s, style guides, photography, videography, 3D and more, with some HTML5 and CSS on the side. In my free time I love to play basketball, work on and drive cars, as well as portrait and automotive photography.", "Hey, I’m Collins!<br>I’m a Project Manager at Cliff Creatives. Bringing an idea to life is no walk in the park. It is my job to make sure expectations are met on both sides - the client and the company. I’m a huge soccer fan (Go Manchester United) and in my spare time I love to cook and watch old sitcoms." ,"Hey, I’m Malek!<br>I’m the back-end developer at Cliff Creatives. I love producing projects for the company! We have lots of fun in the office!I like to spend time with friends, trying new things and exploring new places!"]
-    ;
-    // mateInfo 1 Nick, 2 Collins, 3 Malek;
+    const   mates     = document.querySelectorAll('.mateImg'),
+            bioHead  = document.querySelector('.bioHead'),
+            bio      = document.querySelector('.bio'),
+            mateInfo = ["Hey, I’m Nick. I’m the website designer and front end developer for Cliff Creative’s. I’m a creative mind that focuses heavily on branding with logo design’s, style guides, photography, videography, 3D and more, with some HTML5 and CSS on the side. In my free time I love to play basketball, work on and drive cars, as well as portrait and automotive photography.", 
+                        "Hey, I’m Collins. I’m the Project Manager at Cliff Creatives. Bringing an idea to life is no walk in the park. It is my job to make sure expectations are met on both sides - the client and the company. I’m a huge soccer fan (Go Manchester United) and in my spare time I love to cook and watch old sitcoms." ,
+                        "Hey, I’m Malek. I’m the back-end developer at Cliff Creatives. I love producing projects for the company! We have lots of fun in the office!I like to spend time with friends, trying new things and exploring new places!"];
+    // mateInfo: 0 Nick, 1 Collins, 2 Malek;
+
+
+        function showBio(){
+            let name = this.className.split(" ")[1];
+            
+            if(name == "nick"){
+                bioHead.innerHTML = "I’m Nick";
+                bio.innerHTML = mateInfo[0];
+
+                bioHead.style.textAlign = 'left';
+                bio.style.height = '100px';
+            }
+
+            if(name == "collins"){
+                bioHead.innerHTML = "I’m Collins";
+                bio.innerHTML = mateInfo[1];
+
+                bioHead.style.textAlign = 'center';
+                bio.style.height = '100px';
+            }
+
+            if(name == "malek"){
+                bioHead.innerHTML = "I’m Malek";
+                bio.innerHTML = mateInfo[2];
+
+                bioHead.style.textAlign = 'right';
+                bio.style.height = '100px';
+            }
+        }
+        mates.forEach(mate => mate.addEventListener('click', showBio));
+
+
     })();
