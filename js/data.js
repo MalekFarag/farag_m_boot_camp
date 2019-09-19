99848f8e28c1714d31cab4165db1c803d5f526f5
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-09-19 18:08:50 -0400

    header js changed (need anim), started portfolio page

---
+++ js/data.js
@@ -0,0 +1,22 @@
+// creating data set
+const team = {
+
+    nick : {
+        name: "Nick",
+        bio: "Hey, I’m Nick. I’m the website designer and front end developer for Cliff Creative’s. I’m a creative mind that focuses heavily on branding with logo design’s, style guides, photography, videography, 3D and more, with some HTML5 and CSS on the side. In my free time I love to play basketball, work on and drive cars, as well as portrait and automotive photography."
+    },
+
+    collins : {
+        name: "Collins",
+        bio: "Hey, I’m Collins. I’m the Project Manager at Cliff Creatives. Bringing an idea to life is no walk in the park. It is my job to make sure expectations are met on both sides - the client and the company. I’m a huge soccer fan (Go Manchester United) and in my spare time I love to cook and watch old sitcoms."
+    },
+
+    malek : {
+        name: "Malek",
+        bio: "Hey, I’m Malek. I’m the back-end developer at Cliff Creatives. I love producing projects for the company! We have lots of fun in the office!I like to spend time with friends, trying new things and exploring new places!"
+    }
+}
+
+//exprting data set
+
+export default team;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//import team from "./data.js";
+
 (() => {
     console.log('Fired.');
     
@@ -10,21 +12,30 @@
               console.log('You touched my burger >:(');
               nav.classList.toggle('hidden');
               burger.classList.toggle('open');
-              header.classList.toggle('whitebg');
+              header.classList.toggle('bg');
           }
 
           burger.addEventListener('click', showNav);
     
     
     
-    const   mates    = document.querySelectorAll('.mateImg'),
+    const   close    = document.querySelector('.closeX'),
+            mates    = document.querySelectorAll('.mateImg'),
             bioHead  = document.querySelector('.bioHead'),
             bio      = document.querySelector('.bio'),
             mateInfo = ["Hey, I’m Nick. I’m the website designer and front end developer for Cliff Creative’s. I’m a creative mind that focuses heavily on branding with logo design’s, style guides, photography, videography, 3D and more, with some HTML5 and CSS on the side. In my free time I love to play basketball, work on and drive cars, as well as portrait and automotive photography.", 
                         "Hey, I’m Collins. I’m the Project Manager at Cliff Creatives. Bringing an idea to life is no walk in the park. It is my job to make sure expectations are met on both sides - the client and the company. I’m a huge soccer fan (Go Manchester United) and in my spare time I love to cook and watch old sitcoms." ,
                         "Hey, I’m Malek. I’m the back-end developer at Cliff Creatives. I love producing projects for the company! We have lots of fun in the office!I like to spend time with friends, trying new things and exploring new places!"];
-    // mateInfo: 0 Nick, 1 Collins, 2 Malek;
+    // mateInfo: 0 Nick, 1 Collins, 2 Malek
 
+        function closeBio(){
+            close.classList.add('off');
+            bioHead.innerHTML = "Hello, friend.";
+            bioHead.style.textAlign = "center";
+            bio.innerHTML = " ";
+        }
+
+        close.addEventListener('click', closeBio);
 
         function showBio(){
             let name = this.className.split(" ")[1];
@@ -34,7 +45,7 @@
                 bio.innerHTML = mateInfo[0];
 
                 bioHead.style.textAlign = 'left';
-                bio.style.height = '100px';
+                //bio.style.height = '100px';
             }
 
             if(name == "collins"){
@@ -42,7 +53,7 @@
                 bio.innerHTML = mateInfo[1];
 
                 bioHead.style.textAlign = 'center';
-                bio.style.height = '100px';
+                //bio.style.height = '100px';
             }
 
             if(name == "malek"){
@@ -50,9 +61,12 @@
                 bio.innerHTML = mateInfo[2];
 
                 bioHead.style.textAlign = 'right';
-                bio.style.height = '100px';
+                //bio.style.height = '100px';
             }
+            close.classList.remove('off');
+            console.log('clicked', name);
         }
+
         mates.forEach(mate => mate.addEventListener('click', showBio));
 
 
